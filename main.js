8b2e52199277c9a8291801fe93f58166931083f4
OLGA <olichka23.85@gmail.com>
2021-09-13 15:14:13 -0400

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.getElementById('modal');
+modal.classList.toggle('hidden');
 
-
-
+function setLike(id) {
+  mimicServerCall().then(function () {
+    let heart = document.querySelector('#id201811189 .like-glyph');
+    if (heart.innerHTML == EMPTY_HEART) {
+      heart.classList.add('activated-heart');
+      heart.innerHTML = FULL_HEART;
+    }
+    else {
+      heart.classList.remove('activated-heart');
+      heart.innerHTML = EMPTY_HEART;
+    }
+  }).catch(function ( message ) {
+    modal.classList.toggle('hidden');
+    document.getElementById('modal-message').innerHTML = message;
+    setTimeout(() => {
+      modal.classList.toggle('hidden');      
+    }, 3000);
+  });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
